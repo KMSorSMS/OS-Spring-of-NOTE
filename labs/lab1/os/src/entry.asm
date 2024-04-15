2d51f7ad3425a1054191664e6dcd7a0125eab059
liam <liamyzw@126.com>
2024-04-15 19:41:27 +0800

    learn linker and makefile

--- labs/lab1/os/src/entry.asm
+++ labs/lab1/os/src/entry.asm
@@ -1,4 +1,12 @@
     .section .text.entry
     .global _start
 _start:
-    li x1, 100
+    la sp, boot_stack_top
+    call rust_main
+
+    .section .bss.stack
+    .globl boot_stack_lower_bound
+boot_stack_lower_bound:
+    .space 4096 * 16
+    .globl boot_stack_top
+boot_stack_top:
--- labs/lab1/os/src/main.rs
+++ labs/lab1/os/src/main.rs
@@ -3,7 +3,21 @@
 mod lang_items;
 
 use core::arch::global_asm;
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop {}
+}
 global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
 
 // fn main() {
 //     // println!("Hello, world!");
