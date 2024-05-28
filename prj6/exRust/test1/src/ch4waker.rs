8437d99d9ff03fee0cf8c08c7c32e8f109386dce
liam <liamyzw@126.com>
2024-05-28 20:11:44 +0800

    add prj6

---
+++ prj6/exRust/test1/src/ch4waker.rs
@@ -0,0 +1,59 @@
+use std::mem::{align_of, size_of};
+// A reference to a trait object is a fat pointer: (data_ptr, vtable_ptr)
+trait Test {
+    fn add(&self) -> i32;
+    fn sub(&self) -> i32;
+    fn mul(&self) -> i32;
+}
+
+// This will represent our home-brewed fat pointer to a trait object
+#[repr(C)]
+struct FatPointer<'a> {
+    /// A reference is a pointer to an instantiated `Data` instance
+    data: &'a mut Data,
+    /// Since we need to pass in literal values like length and alignment it's
+    /// easiest for us to convert pointers to usize-integers instead of the other way around.
+    vtable: *const usize,
+}
+
+// This is the data in our trait object. It's just two numbers we want to operate on.
+struct Data {
+    a: i32,
+    b: i32,
+}
+
+// ====== function definitions ======
+fn add(s: &Data) -> i32 {
+    s.a + s.b
+}
+fn sub(s: &Data) -> i32 {
+    s.a - s.b
+}
+fn mul(s: &Data) -> i32 {
+    s.a * s.b
+}
+
+pub fn main_ch4() {
+    let mut data = Data {a: 3, b: 2};
+    // vtable is like special purpose array of pointer-length types with a fixed
+    // format where the three first values contains some general information like
+    // a pointer to drop and the length and data alignment of `data`.
+    let vtable = vec![
+        0,                  // pointer to `Drop` (which we're not implementing here)
+        size_of::<Data>(),  // length of data
+        align_of::<Data>(), // alignment of data
+
+        // we need to make sure we add these in the same order as defined in the Trait.
+        add as usize, // function pointer - try changing the order of `add`
+        sub as usize, // function pointer - and `sub` to see what happens
+        mul as usize, // function pointer
+    ];
+
+    let fat_pointer = FatPointer { data: &mut data, vtable: vtable.as_ptr()};
+    let test = unsafe { std::mem::transmute::<FatPointer, &dyn Test>(fat_pointer) };
+
+    // And voalá, it's now a trait object we can call methods on
+    println!("Add: 3 + 2 = {}", test.add());
+    println!("Sub: 3 - 2 = {}", test.sub());
+    println!("Mul: 3 * 2 = {}", test.mul());
+}
---
+++ prj6/exRust/test1/src/main.rs
@@ -0,0 +1,69 @@
+// use futures::executor::block_on;
+
+// async fn hello_world() {
+//     println!("hello, world!");
+// }
+
+// fn main() {
+//     let future = hello_world();
+//     block_on(future);
+// }
+
+// use futures::executor::block_on;
+
+
+// async fn learn_and_sing() {
+//     // Wait until the song has been learned before singing it.
+//     // We use `.await` here rather than `block_on` to prevent blocking the
+//     // thread, which makes it possible to `dance` at the same time.
+//     let song = learn_song().await;
+//     sing_song(song).await;
+// }
+// async fn learn_song() -> String {
+//     "I'm singing a song".to_string()
+// }
+// async fn sing_song(song: String) {
+//     println!("{}", song);
+// }
+// async fn dance() {
+//     println!("I'm dancing");
+// }
+
+// async fn async_main() {
+//     let f1 = learn_and_sing();
+//     let f2 = dance();
+
+//     // `join!` is like `.await` but can wait for multiple futures concurrently.
+//     // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
+//     // future will take over the current thread. If `dance` becomes blocked,
+//     // `learn_and_sing` can take back over. If both futures are blocked, then
+//     // `async_main` is blocked and will yield to the executor.
+//     futures::join!(f1, f2);
+// }
+
+// fn main() {
+//     block_on(async_main());
+// }
+
+// use std::mem::size_of;
+// trait SomeTrait { }
+
+use ch4waker::main_ch4;
+
+// fn main() {
+//     println!("======== The size of different pointers in Rust: ========");
+//     println!("&dyn Trait:------{}", size_of::<&dyn SomeTrait>());
+//     println!("&[&dyn Trait]:---{}", size_of::<&[&dyn SomeTrait]>());
+//     println!("Box<dyn Trait>:------{}", size_of::<Box<dyn SomeTrait>>());
+//     println!("Box<Box<dyn Trait>>:-{}", size_of::<Box<Box<dyn SomeTrait>>>());
+//     println!("&i32:------------{}", size_of::<&i32>());
+//     println!("&[i32]:----------{}", size_of::<&[i32]>());
+//     println!("Box<i32>:--------{}", size_of::<Box<i32>>());
+//     println!("&Box<i32>:-------{}", size_of::<&Box<i32>>());
+//     println!("[&dyn Trait;4]:--{}", size_of::<[&dyn SomeTrait; 4]>());
+//     println!("[i32;4]:---------{}", size_of::<[i32; 4]>());
+// }
+mod ch4waker;
+fn main(){
+    main_ch4();
+}
